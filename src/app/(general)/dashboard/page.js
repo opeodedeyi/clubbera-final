91df145f91e0733c5d1cfc3f6e71f23bffea24b6
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2024-06-18 11:26:13 +0100

    half done the refactoring

--- src/app/(general)/dashboard/page.js
+++ src/app/(general)/dashboard/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import { useUserSession } from "@/hooks/useUserSession";
 import DashboardContent from "@/app/(general)/dashboard/DashboardContent";
 import "@/app/style/homepage.css"
