65a9506a8a05d072347ef56ddc7e6c57d1e1bb66
Demi-la <adebiyio28@gmail.com>
2024-06-29 15:05:51 +0100

    added validation messages

--- src/app/(auth)/signup/SignupStepTwo.js
+++ src/app/(auth)/signup/SignupStepTwo.js
@@ -4,37 +4,72 @@ import CheckboxInput from "@/components/forminput/CheckBoxInput/CheckBoxInput";
 import CustomButton from "@/components/utility/CustomButton/CustomButton";
 import style from "../Auth.module.css";
 
-
-export default function SignupStepTwo ({ ageConsent, setAgeConsent, cityLocation, setCityLocation, setLatLocation, setLngLocation, buttonClicked }) {
-    return (
-        <>
-            <div className={style.authFormContent}>
-                <div className={style.authFormContentMain}>
-                    <div className={style.authFormContentIntro}>
-                        <h3>Finish signing up</h3>
-                        <p className={style.authFormContentIntroText}>We just need a few information from you and your account will be all set up.</p>
-                    </div>
-                </div>
-            </div>
-            <div className={style.authFormInputs}>
-                <CityInput 
-                    label="Location" 
-                    placeholder="Enter city" 
-                    cityLocation={cityLocation} 
-                    setCityLocation={setCityLocation} 
-                    setLatLocation={setLatLocation}
-                    setLngLocation={setLngLocation}/>
-                <CheckboxInput
-                    label="Age"
-                    checked={ageConsent} 
-                    onChange={() => setAgeConsent(prev => !prev)}>
-                    I confirm I am 18 years of age or older
-                </CheckboxInput>
-            </div>
-            <div className={style.authFormActions}>
-                <CustomButton size="fullwidthSize" disabled={!ageConsent} onClick={buttonClicked}>Get started</CustomButton>
-                <p className={style.authFormActionsPolicy}>By signing up, you agree to <Link href="/termsofservice" className="auth-dark-link">Terms of Service</Link>, <Link href="/privacypolicy" className="auth-dark-link">Privacy Policy</Link>, and <Link href="/cookiepolicy" className="auth-dark-link">Cookie Policy</Link>.</p>
-            </div>
-        </>
-    );
+export default function SignupStepTwo({
+  ageConsent,
+  setAgeConsent,
+  cityLocation,
+  setCityLocation,
+  setLatLocation,
+  setLngLocation,
+  buttonClicked,
+  errors,
+}) {
+  return (
+    <>
+      <div className={style.authFormContent}>
+        <div className={style.authFormContentMain}>
+          <div className={style.authFormContentIntro}>
+            <h3>Finish signing up</h3>
+            <p className={style.authFormContentIntroText}>
+              We just need a few information from you and your account will be
+              all set up.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={style.authFormInputs}>
+        <CityInput
+          label="Location"
+          placeholder="Enter city"
+          cityLocation={cityLocation}
+          setCityLocation={setCityLocation}
+          setLatLocation={setLatLocation}
+          setLngLocation={setLngLocation}
+        />
+        {errors.location && (
+          <span className={style.errorMessage}>{errors.location}</span>
+        )}
+        <CheckboxInput
+          label="Age"
+          checked={ageConsent}
+          onChange={() => setAgeConsent((prev) => !prev)}
+        >
+          I confirm I am 18 years of age or older
+        </CheckboxInput>
+        {errors.ageConsent && (
+          <span className={style.errorMessage}>{errors.ageConsent}</span>
+        )}
+      </div>
+      <div className={style.authFormActions}>
+        <CustomButton size="fullwidthSize" onClick={buttonClicked}>
+          Get started
+        </CustomButton>
+        <p className={style.authFormActionsPolicy}>
+          By signing up, you agree to{" "}
+          <Link href="/termsofservice" className="auth-dark-link">
+            Terms of Service
+          </Link>
+          ,{" "}
+          <Link href="/privacypolicy" className="auth-dark-link">
+            Privacy Policy
+          </Link>
+          , and{" "}
+          <Link href="/cookiepolicy" className="auth-dark-link">
+            Cookie Policy
+          </Link>
+          .
+        </p>
+      </div>
+    </>
+  );
 }
--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,59 +1,76 @@
-'use client';
+"use client";
 
 import SignupStepOne from "@/app/(auth)/signup/SignupStepOne";
 import SignupStepTwo from "@/app/(auth)/signup/SignupStepTwo";
 import useSignupForm from "@/hooks/useSignupForm";
 import style from "../Auth.module.css";
 
-
 export default function Signup({ searchParams }) {
-    const { destination } = searchParams
-    const {
-        step,
-        setStep,
-        email,
-        setEmail,
-        fullName,
-        setFullName,
-        password,
-        setPassword,
-        cityLocation,
-        setCityLocation,
-        setLatLocation,
-        setLngLocation,
-        ageConsent,
-        setAgeConsent,
-        isDisabled,
-        handleSignup
-    } = useSignupForm(destination);
+  const { destination } = searchParams;
+  const {
+    step,
+    setStep,
+    email,
+    setEmail,
+    fullName,
+    setFullName,
+    password,
+    setPassword,
+    cityLocation,
+    setCityLocation,
+    setLatLocation,
+    setLngLocation,
+    ageConsent,
+    setAgeConsent,
+    isDisabled,
+    handleSignup,
+    errors,
+    validateStepOne,
+  } = useSignupForm(destination);
 
-    return (
-        <>
-            <div className={style.authContainer}>
-                <form className={style.authContainerMain}>
-                    {step===1 ?
-                        <SignupStepOne
-                            email={email} 
-                            setEmail={setEmail} 
-                            fullName={fullName} 
-                            setFullName={setFullName} 
-                            password={password} 
-                            setPassword={setPassword}
-                            isDisabled={isDisabled}
-                            buttonClicked={() => setStep(2)}/>
-                    :
-                        <SignupStepTwo
-                            ageConsent={ageConsent} 
-                            setAgeConsent={setAgeConsent}
-                            cityLocation={cityLocation}
-                            setCityLocation={setCityLocation} 
-                            setLatLocation={setLatLocation}
-                            setLngLocation={setLngLocation}
-                            buttonClicked={handleSignup}/>
-                    }
-                </form>
-            </div>
-        </>
-    )
-}
+  const moveToNextStep = () => {
+    if (step === 1) {
+      const isValidStepOne = validateStepOne();
+      if (isValidStepOne) {
+        setStep(2);
+      }
+    } else {
+      setStep(2);
+    }
+  };
   
+
+  return (
+    <>
+      <div className={style.authContainer}>
+        <form className={style.authContainerMain}>
+          {step === 1 ? (
+            <SignupStepOne
+              email={email}
+              setEmail={setEmail}
+              fullName={fullName}
+              setFullName={setFullName}
+              password={password}
+              setPassword={setPassword}
+              errors={errors}
+              isDisabled={isDisabled}
+              buttonClicked={() => setStep(2)}
+              moveToNextStep={moveToNextStep}
+            />
+          ) : (
+            <SignupStepTwo
+              ageConsent={ageConsent}
+              setAgeConsent={setAgeConsent}
+              cityLocation={cityLocation}
+              setCityLocation={setCityLocation}
+              setLatLocation={setLatLocation}
+              setLngLocation={setLngLocation}
+              buttonClicked={handleSignup}
+              errors={errors}
+            />
+          )}
+        </form>
+      </div>
+    </>
+  );
+}
