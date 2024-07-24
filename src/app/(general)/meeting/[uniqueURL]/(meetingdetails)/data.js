bbb3b078059ccced49f8803682bc571070f57ec8
Demi-la <adebiyio28@gmail.com>
2024-07-24 16:34:10 +0100

    completed meeting details page

---
+++ src/app/(general)/meeting/[uniqueURL]/(meetingdetails)/data.js
@@ -0,0 +1,19 @@
+import attendee1 from "../../../../../../public/attendee1.png"
+import attendee2 from "../../../../../../public/attendee2.png";
+export const attendees = [
+  {
+    attendeeImage: attendee1,
+  },
+  {
+    attendeeImage: attendee2,
+  },
+  {
+    attendeeImage: attendee1,
+  },
+  {
+    attendeeImage: attendee2,
+  },
+  {
+    attendeeImage: attendee1,
+  },
+];
---
+++ src/app/(general)/meeting/[uniqueURL]/(meetingdetails)/hangoutDescription.js
@@ -0,0 +1,77 @@
+import meetingHeroImage from "../../../../../../public/meetingHeroImage.png";
+import mobileHangoutHero from "../../../../../../public/mobileHangoutHero.png";
+import bookmark from "../../../../../../public/bookmark.svg";
+import Image from "next/image";
+import style from "./hangoutDescription.module.css";
+import { attendees } from "./data";
+import CustomButton from "@/components/utility/CustomButton/CustomButton";
+import { IoShareSocialOutline } from "react-icons/io5";
+const HangoutDescription = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <Image
+            src={meetingHeroImage}
+            alt="Hero Image"
+            height={"100%"}
+            width={"100%"}
+            className={style.heroImage}
+          />
+        </div>
+        <div>
+          <Image
+            src={mobileHangoutHero}
+            alt="Hero Image"
+            height={"100%"}
+            width={"100%"}
+            className={style.MobileheroImage}
+          />
+        </div>
+        <div className={style.hangoutContainer}>
+          <div className={style.hangoutDetailsContainer}>
+            <p className={style.hangoutTitle}>Basketbal Hangout</p>
+            <div className={style.attendeeImages}>
+              {attendees.map((attendee, index) => (
+                <div key={index}>
+                  <Image
+                    src={attendee.attendeeImage}
+                    alt="Attendee"
+                    height={"100%"}
+                    width={"100%"}
+                  />
+                </div>
+              ))}
+              <p className={style.totalAttendees}>12 Attendees</p>
+            </div>
+            <p className={style.hangoutDescription}>
+              This is a Basketball group and a group for basketball lovers. We
+              usually meet up at the London Basketball field, near Jameson Park,
+              every Saturday.
+            </p>
+          </div>
+          <div className={style.rsvpContainer}>
+            <div>
+              <p>Ensure to RSVP</p>
+              <div className={style.rsvpButton}>
+                <CustomButton>Attend Event</CustomButton>
+              </div>
+            </div>
+            <hr className={style.horizontalLine} />
+            <div>
+              <IoShareSocialOutline />
+              <Image
+                src={bookmark}
+                alt="Bookmark"
+                height={"2rem"}
+                width={"2rem"}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HangoutDescription;
